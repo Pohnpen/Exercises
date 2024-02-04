e2adcb77c4e23d78b65d745c0f47a8b79535bf85
Philipp Rollinger <philipp.rollinger@gmail.com>
2024-02-04 14:53:39 +0700

    your first exercise.

---
+++ guess_the_number.py
@@ -0,0 +1,6 @@
+# Let's write a program where the user has x trials to guess the a random number between min_n and max_n.
+# If the user has correctly guessed the user wins, otherwise the user looses.
+
+X = 10
+MIN_N = 1
+MAX_N = 100
