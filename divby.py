9f7b2774fa149429413f2442eddb4094314967b3
Philipp Rollinger <philipp.rollinger@gmail.com>
2024-02-04 15:36:08 +0700

    some tests for divby

--- divby.py
+++ divby.py
@@ -1,9 +1,16 @@
 def divby(n,d):
     """
-    Returns True if the number n is divisible by the divisor d.
+    Returns True if the number n is divisible by the divisor d, otherwise returns False
 
     @params:
     n := number (int, float)
     d := divisor (int, float)
     """
     pass
+
+if __name__ == "__main__":
+    assert divby(5,2) == False
+    assert divby(5,2.5) == True
+    assert divby(99.6, 100) == False
+    assert divby(55,5) == True
+
