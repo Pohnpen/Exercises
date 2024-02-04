2382cb8475a1f721fcbda9c033adfcc2caa9e26a
Philipp Rollinger <philipp.rollinger@gmail.com>
2024-02-04 16:13:04 +0700

    next task

--- conditional_print.py
+++ conditional_print.py
@@ -5,3 +5,5 @@ The number must be divisible by five
 If the number is greater than 150, then skip it and move to the next number
 If the number is greater than 500, then stop the loop
 """
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
