0c9d6cd54b5d7a8c73cca8d0d57c1ad05e660ff5
Philipp Rollinger <philipp.rollinger@gmail.com>
2024-02-04 15:27:26 +0700

    divby excercise

---
+++ divby.py
@@ -0,0 +1,9 @@
+def divby(n,d):
+    """
+    Returns True if the number n is divisible by the divisor d.
+
+    @params:
+    n := number (int, float)
+    d := divisor (int, float)
+    """
+    pass
