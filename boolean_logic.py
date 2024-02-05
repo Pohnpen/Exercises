469c09a1bbc893d9bebccf915cb2f76ea2ac0e34
Philipp Rollinger <philipp.rollinger@gmail.com>
2024-02-05 18:12:21 +0700

    new version of boolean logic

--- boolean_logic.py
+++ boolean_logic.py
@@ -1,21 +1,31 @@
 def boolean_exercise_1(x,y):
     """
-    1. Give a Boolean formula using only AND, OR, and NOT for the function
-        of two Boolean variables x and y that is true if and only if x = y.
-        Give the corresponding truth table and draw the circuit.
+    Give a Boolean formula using only AND, OR, and NOT for the function
+    of two Boolean variables x and y that is true if and only if x = y.
+    Give the corresponding truth table and draw the circuit.
     """
     pass
 
-def boolean_exercise_1(x,y,z):
+def boolean_exercise_2(x,y,z):
     """
-    Draw a Boolean circuit using AND, OR, and NOT gates that implements
+    Create a boolean function using AND, OR, and NOT gates that implements
     the function of three variables that is 1 if all three inputs are
     the same, and 0 otherwise.
     """
+    pass
 
-def boolean_exercise_1(x,y,z):
+def boolean_exercise_3(a,b,c,d):
     """
-    Draw a Boolean circuit using AND, OR, and NOT gates that implements
+    Create a boolean function using AND, OR, and NOT gates that implements
+    the function of four variables that is 1 if precisely two of the
+    variables have the same value, 0 otherwise.
+    """
+    pass
+
+def boolean_exercise_4(k,j,i):
+    """
+    Create a boolean function using AND, OR, and NOT gates that implements
     the function of three variables that is 0 if all three inputs are
-    the same, and 1 otherwise. 
+    the same, and 1 otherwise.
     """
+    pass
